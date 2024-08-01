638c8b805c5b12eaf3d3cf45c7c0fe4f944ad4b1
hepzishanthijohn <yyepsipal@gmail.com>
2024-08-01 21:36:35 +0530

    second commit

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -84,11 +84,7 @@ router.post("/login", async (req, res) => {
       { expiresIn: "1h" },
       (err, token) => {
         if (err) throw err;
-        res.json({ token,
-            
-                 username,
-                email,
-                 _id,
+        res.json({ token,username:user.username,email:user.email,id:user. _id,
               
         });
       }
