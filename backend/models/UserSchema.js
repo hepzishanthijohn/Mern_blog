6082e37ee9707de9c3880db0f0a0ae76e7df1963
hepzishanthijohn <yyepsipal@gmail.com>
2024-08-01 21:00:37 +0530

    first commit

--- backend/models/UserSchema.js
+++ backend/models/UserSchema.js
@@ -1,12 +1,22 @@
-const mongoose = require('mongoose');
-const {Schema, model} = mongoose;
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
-const UserSchema = new Schema({
-  username: {type: String, required: true, min: 4, },
-  email:{type: String, required: true, unique: true},
-  password: {type: String, required: true},
+const UserSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, default: "users" },
 });
 
-const User = model('User', UserSchema);
+// Pre-save hook to hash password
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+const User = mongoose.model("User", UserSchema);
 
 module.exports = User;
